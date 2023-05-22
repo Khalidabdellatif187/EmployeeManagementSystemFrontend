e7f80776c54c4064701a5cabc5ac399495895bc9
khalid187 <khalidabdellatif187@gmail.com>
2023-05-23 00:21:49 +0300

    Add And Rout Components In app js

--- employee-management-system-frontend/src/App.js
+++ employee-management-system-frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import EmployeeCrudOperationsComponent from './components/EmployeeCrudOperationsComponent';
+import HeaderComponent from './components/HeaderComponent';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+      <HeaderComponent/>
+      <div className="container">
+        <Routes>
+          <Route exact path="/" Component={ListEmployeeComponent}></Route>
+          <Route path="/employees" Component={ListEmployeeComponent}></Route>
+          <Route path="/add-employee" Component={EmployeeCrudOperationsComponent }></Route>
+          <Route path="/edit-employee/:id" Component={EmployeeCrudOperationsComponent }></Route>
+        </Routes>
+      </div>
+      </Router>
     </div>
   );
 }
